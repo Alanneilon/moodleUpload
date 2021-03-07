d614315698fe3dfc17a2420fa4b5231058414f36
Alanneilon <alanneilon@gmail.com>
2021-03-07 16:32:01 +0000

    tidiedUpCode&comments

--- script.py
+++ script.py
@@ -53,58 +53,65 @@ def call(fname, **kwargs):
     if type(response) == dict and response.get('exception'):
         raise SystemError("Error calling Moodle API\n", response)
     return response
-
+#################################################################################################################
 def getNoFromFolder(filelist):
+#function to extract numbers from folder and filenames
     wkno=[]
     for i in range(0, len(filelist)):
         print(filelist[i])
+        #extract number from each file name in the list
         wk=re.findall('[0-9]+', filelist[i])
+        #display the number
         print(wk)
+        #add the number to a list that will store just the numbers
         wkno.extend(wk)
     return wkno
-
+##################################################################################################################
 def convertStringToInt(stringList):
+#function to convert the numbers from the files names to an int, as they are caputured as string
     n = 0
     while n < len(stringList):
         stringList[n] = int(stringList[n])
         n += 1
+        #return the list of integers
     return(stringList)
-
+####################################################################################################################################################################################################################
 def updateLinks(wkno):
+    #assemble the links and link names that will go in the summary of each required section
     summaryList=[]
     for i in range(0, len(wkno)):
         no=wkno[i]
+        # using place holders to implant the numbers(stored as string) into the links
         summary="<a href=\'https://mikhail-cct.github.io/ooapp/wk{}/#\'>Week {}: Data types</a><br><a href=\"https://mikhail-cct.github.io/ca3-test/wk{}.pdf\">Week {}: Modules.pdf</a>".format(no,no,no,no)
+        # assing the summary links and names to a new list and then return it to the caller        
         summaryList.append(summary)
     return summaryList
-
+####################################################################################################################################################################################################################
 def updateAllSec(courseid,data,wknum,sumList):
-
+#function to update all required summary sections that match the incoming sectionid with links 
     for i in range(0, len(wknum)):
         data[0]['summary'] = sumList[i]
         data[0]['section'] = wknum[i]
+        # writing the links each section one at a time until loop completes
         sec_write = LocalUpdateSections(courseid, data)
 
+####################################################################################################################
+def getLocalDirItems():
+# function to get a list of items in the local directory
+    filelist=os.listdir()
+    print(filelist)
+    return filelist
 
-
-
-    
-
-
-
-
-################################################
+######################################################################################################################
 # Rest-Api classes
-################################################
-
-
+#####################################################################################################################
 class LocalGetSections(object):
     """Get settings of sections. Requires courseid. Optional you can specify sections via number or id."""
 
     def __init__(self, cid, secnums=[], secids=[]):
         self.getsections = call('local_wsmanagesections_get_sections',
                                 courseid=cid, sectionnumbers=secnums, sectionids=secids)
-
+###################################################################################################################
 
 class LocalUpdateSections(object):
     """Updates sectionnames. Requires: courseid and an array with sectionnumbers and sectionnames"""
@@ -113,12 +120,9 @@ class LocalUpdateSections(object):
         self.updatesections = call(
             'local_wsmanagesections_update_sections', courseid=cid, sections=sectionsdata)
 
-################################################
-# Example
-################################################
+######################################################################################################################
 
-
-courseid = "17"  # Exchange with valid id.
+courseid = "17"  # id of course required to be updated
 
 # Get all sections of the course.
 sec = LocalGetSections(courseid)
@@ -128,70 +132,28 @@ data = [{'type': 'num', 'section':0 , 'summary': '', 'summaryformat': 1, 'visibl
 'highlight': 0, 'sectionformatoptions': [{'name': 'level', 'value': '1'}]}]
 
 # Output readable JSON, but print only summary
-#print(json.dumps(sec.getsections[10], indent=4, sort_keys=True))
 print(json.dumps(sec.getsections, indent=4, sort_keys=True))
-
-
-#summary="<a href=\'https://mikhail-cct.github.io/ooapp/wk2/#\'>Week 2: Data types</a><br>"
-#section=2
-
-
-
-
-
-# Assign the correct summary
-#data[0]['summary'] = summary
-#data[0]['section'] = section
-
-#sec = LocalGetSections(courseid)
-
-#sec_write = LocalUpdateSections(courseid, data)
-
-path=os.getcwd()
-filelist=os.listdir()
-print(path)
-print(filelist)
-
-
-
-
+#get list of local folders and files
+filelist=getLocalDirItems()
+#extract a number from the name of the folders and files
 wkno=getNoFromFolder(filelist)
-print("Hello")
+#print a list of the numbers from the names of the files
 print(wkno)  
-print(type(wkno[0]))
-print("Hi")
+#convert the numbers on the list extracted from the file names to int
 wknum=convertStringToInt(wkno)
+#print the list of int that will set the section id's
 print(wknum)
-print(type(wknum[0]))
+#assemble the summarysummaries with the required links that match the section id's
 sumList=updateLinks(wkno)
+#display the links assembled
 print(sumList)
+#update the sections on the website with the summaries that match the required section number
 updateSec=updateAllSec(courseid,data,wknum,sumList)
+#obtain the newly updated section information
 sec = LocalGetSections(courseid)
+#display the latest section information
 print(json.dumps(sec.getsections, indent=4, sort_keys=True))
 
 
-"""
-# Split the section name by dash and convert the date into the timestamp, it takes the current year, so think of a way for making sure it has the correct year!
-month = parser.parse(list(sec.getsections)[1]['name'].split('-')[0])
-# Show the resulting timestamp
-print(month)
-# Extract the week number from the start of the calendar year
-print(month.strftime("%V"))
 
-#  Assemble the payload
-data = [{'type': 'num', 'section': 0, 'summary': '', 'summaryformat': 1, 'visible': 1 , 'highlight': 0, 'sectionformatoptions': [{'name': 'level', 'value': '1'}]}]
 
-# Assemble the correct summary
-summary = '<a href="https://mikhail-cct.github.io/ca3-test/wk1/">Week 1: Introduction</a><br>'
-
-# Assign the correct summary
-data[0]['summary'] = summary
-
-# Set the correct section number
-data[0]['section'] = 1
-
-# Write the data back to Moodle
-sec_write = LocalUpdateSections(courseid, data)
-
-sec = LocalGetSections(courseid)
-print(json.dumps(sec.getsections[1]['summary'], indent=4, sort_keys=True))"""
